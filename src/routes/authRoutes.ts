da188f6e81883fff728d7bf76f465ff3911b4cc7
nono <nour98nour@gmail.com>
2026-07-14 20:10:32 +0300

    feat: enhance authRoutes with validation and authentication middleware for user registration, login, logout, profile picture upload, and password management

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -6,20 +6,38 @@ import {
   uploadProfilePic,
   forgetPassword,
   resetPassword,
+  getProfile,
 } from '../controllers/authController';
+import authenticate from '../middlewares/auth';
+import validate from '../middlewares/validate';
+import { uploadProfilePic as uploadMiddleware } from '../middlewares/upload';
+import {
+  registerSchema,
+  loginSchema,
+  forgotPasswordSchema,
+  resetPasswordSchema,
+} from '../validations/authValidation';
 
 const router = express.Router();
 
-router.route('/register').post(registerUser);
+router.route('/register').post(validate(registerSchema), registerUser);
 
-router.route('/login').post(loginUser);
+router.route('/login').post(validate(loginSchema), loginUser);
 
-router.route('/logout').post(logoutUser);
+router.route('/logout').post(authenticate, logoutUser);
 
-router.route('/uplaod-profile-pic').patch(uploadProfilePic);
+router
+  .route('/upload-profile-pic')
+  .patch(authenticate, uploadMiddleware, uploadProfilePic);
 
-router.route('/forget-password').post(forgetPassword);
+router
+  .route('/forget-password')
+  .post(validate(forgotPasswordSchema), forgetPassword);
 
-router.route('/reset-password').post(resetPassword);
+router
+  .route('/reset-password')
+  .post(validate(resetPasswordSchema), resetPassword);
+
+router.route('/profile').get(authenticate, getProfile);
 
 export default router;
